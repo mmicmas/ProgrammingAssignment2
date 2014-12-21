30e40b2b66bf753405bfa6e7a076a1a7f61a7661
Michal <mmicmas@gmail.com>
2014-12-21 21:56:58 +0100

    R assignment 2 - final, working version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The followning method creates matrix that will be cached. Cache is emptied when set is called
+## Returns the list containsing method of set / get Inversed matrix 
 makeCacheMatrix <- function(x = matrix()) {
+  
+    inversed <- NULL
 
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        inversed <<- NULL 
+    }
+
+    get <- function() x
+
+    # set inverse of matrix
+    setInversed <- function(inverse) inversed <<- inverse
+
+    # get inverse of matrix
+    getInversed <- function() inversed
+
+    list(set = set, get = get,
+        setInversed = setInversed,
+        getInversed = getInversed)
 }
 
 
-## Write a short comment describing this function
-
+## Inversing the matrix is heavy and time consuming operation. Therefore, if possible, it is cached by the methof above
+## which does the inversion. The following method uses the methods from the makeCacheMatrix. It uses the scoped vars between two methods
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inversed <- x$getInversed()    
+    
+    if(!is.null(inversed)) {
+        ##get cached and return
+        return(inversed)
+    }
+
+    data <- x$get()
+
+	## no cahce was available, so do it now and return computed matrix
+    inversed <- solve(data, ...)	
+    x$setInversed(inversed)
+    inversed
 }
